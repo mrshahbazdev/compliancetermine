ce09662c42f4ab35669cbc5e4a1aa02f046e0597
unknown <mr.shahbaznns@gmail.com>
2025-12-21 12:28:45 +0500

    calender update

--- resources/views/layouts/tenant.blade.php
+++ resources/views/layouts/tenant.blade.php
@@ -7,7 +7,7 @@
     
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
         * { font-family: 'Inter', sans-serif; }
